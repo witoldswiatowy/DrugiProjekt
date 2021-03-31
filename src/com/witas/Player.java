7a6545182bf471b2155faac147760296762ec9bc
Witold Swiatowy <swiatowywitold@gmail.com>
2021-04-01 00:41:27 +0200

    Commit do Mastera po branchu

--- src/com/witas/Player.java
+++ src/com/witas/Player.java
@@ -27,37 +27,49 @@ public class Player {
     }
 
     void createPlayer (){
-//        System.out.println("Jakiej rasy jest twoja postać? \n Człowiek, Krasnolud czy Ogr?");
-//        String choiceSpecies = scanner.next();
-//        switch (choiceSpecies){
-//            case "Człowiek":
-//                this.species = Species.CZLOWIEK;
-//                break;
-//            case "Krasnolud":
-//                this.species = Species.KRASNOLUD;
-//                break;
-//            case "Ogr":
-//                this.species = Species.OGR;
-//                break;
-//            default:
-//                System.out.println("Chyba umiesz poprawnie napisać nazwę swojej rasy? Spróbuj jeszcze raz.");
-//        }
-//        System.out.println("Ok, twoja postać to: " + this.species);
-//        System.out.println("Jak nazywa się twoja postać?");
-//        this.name = scanner.next();
+        System.out.println("Jakiej rasy jest twoja postać? \n Człowiek, Krasnolud czy Ogr?");
+        String choiceSpecies = scanner.next();
+        switch (choiceSpecies){
+            case "Człowiek":
+                this.species = Species.CZLOWIEK;
+                break;
+            case "Krasnolud":
+                this.species = Species.KRASNOLUD;
+                break;
+            case "Ogr":
+                this.species = Species.OGR;
+                break;
+            default:
+                System.out.println("Chyba umiesz poprawnie napisać nazwę swojej rasy? Spróbuj jeszcze raz.");
+        }
+        System.out.println("Ok, twoja postać to: " + this.species);
+        System.out.println("Jak nazywa się twoja postać?");
+        this.name = scanner.next();
 
         System.out.println("Ile ma defa? (Wpisz L, by wylosować)");
         String def = scanner.next();
         if (def.equals("L")){
             this.def = rnd.nextInt(10) + 1;
-        System.out.println("Twoja postać ma: " + this.def + " defa.");
+        System.out.println("Twoja postać ma: " + this.def + " obrony.");
         }
-        System.out.println("Ile ma hp?");
-        this.hp = scanner.nextInt();
-        System.out.println("Ile ma power?");
-        this.power = scanner.nextInt();
-        System.out.println("Ile ma ini?");
-        this.ini = scanner.nextInt();
+        System.out.println("Ile ma hp? (Wpisz L, by wylosować)");
+        String hp = scanner.next();
+        if (hp.equals("L")){
+            this.hp = rnd.nextInt(10) + 1;
+            System.out.println("Twoja postać ma: " + this.hp + " punktów życia.");
+        }
+        System.out.println("Ile ma power? (Wpisz L, by wylosować)");
+        String power = scanner.next();
+        if (power.equals("L")){
+            this.power = rnd.nextInt(10) + 1;
+            System.out.println("Twoja postać ma: " + this.power + " siły.");
+        }
+        System.out.println("Ile ma ini? (Wpisz L, by wylosować)");
+        String ini = scanner.next();
+        if (ini.equals("L")){
+            this.ini = rnd.nextInt(10) + 1;
+            System.out.println("Twoja postać ma: " + this.ini + " inicjatywy.");
+        }
 
         System.out.println(this.name);
         System.out.println(this.def);
--- src/com/witas/Species.java
+++ src/com/witas/Species.java
@@ -5,7 +5,7 @@ public enum Species {
     KRASNOLUD,
     OGR
 }
-
+//0.Obsłużyć błędy przy wpisywaniu do konsoli.
 //1.zrobić ograniczenia do podawanych wartości przy tworzeniu podstaci.
 //  Może tak jak w prawdziwym Warhammerze że każda rasa ma sjkakieś plusy i reszta losuje
 //2.zrobić polecenia z konsoli, np. zaatakuje ogra i wtedy wchodzi w pętlę walki
